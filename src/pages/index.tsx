59177bf03e405f819b12f38780846d3eb4f61573
Callum McIntyre <callum@pointer.gg>
2022-06-01 12:24:09 +0100

    Add a toggle to use/not use microblocks

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -4,12 +4,20 @@ import { useEffect, useState } from "react";
 import Auth from "../components/Auth";
 import { appDetails, contractOwnerAddress } from "../lib/constants";
 import { useStacks } from "../providers/StacksProvider";
+import { Switch } from '@headlessui/react'
+
+function classNames(...classes) {
+  return classes.filter(Boolean).join(' ')
+}
 
 export default function Home() {
   const { network, address } = useStacks();
+  const [useMicrostacks, setUseMicrostacks] = useState(true);
   const [transactionOptions, setTransactionOptions] = useState<ContractCallRegularOptions | undefined>(undefined);
   const [transactionId, setTransactionId] = useState<string | undefined>(undefined)
   const [transactionJson, setTransactionJson] = useState<any>(undefined)
+
+  const anchorMode: AnchorMode = useMicrostacks ? AnchorMode.OffChainOnly : AnchorMode.OnChainOnly
 
   async function fetchTransactionJson() {
     if (!transactionId) return;
@@ -45,7 +53,7 @@ export default function Home() {
       ],
       network,
       appDetails,
-      anchorMode: AnchorMode.OffChainOnly,
+      anchorMode,
       onFinish: ({ txId }) => {
         setTransactionId(txId)
       },
@@ -59,6 +67,29 @@ export default function Home() {
   return (
     <main className="container flex flex-col items-center justify-center min-h-screen gap-16 p-4 mx-auto">
       <Auth />
+
+      <Switch.Group as="div" className="flex items-center">
+        <Switch
+          checked={useMicrostacks}
+          onChange={setUseMicrostacks}
+          className={classNames(
+            useMicrostacks ? 'bg-indigo-600' : 'bg-gray-200',
+            'relative inline-flex flex-shrink-0 h-6 w-11 border-2 border-transparent rounded-full cursor-pointer transition-colors ease-in-out duration-200 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500'
+          )}
+        >
+          <span
+            aria-hidden="true"
+            className={classNames(
+              useMicrostacks ? 'translate-x-5' : 'translate-x-0',
+              'pointer-events-none inline-block h-5 w-5 rounded-full bg-white shadow transform ring-0 transition ease-in-out duration-200'
+            )}
+          />
+        </Switch>
+        <Switch.Label as="span" className="flex flex-col ml-3">
+          <span className="text-sm font-medium text-gray-900">Use Microblocks</span>
+          <span className="text-sm text-gray-500">Anchor mode: {anchorMode}</span>
+        </Switch.Label>
+      </Switch.Group>
 
       <button type="button" disabled={!address} className="px-4 py-2 font-bold text-white bg-blue-500 rounded hover:bg-blue-700 disabled:opacity-50 disabled:cursor-not-allowed" onClick={mintTokens}>Mint Tokens</button>
 
